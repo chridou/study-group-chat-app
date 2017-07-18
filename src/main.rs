846e83dc5e3fff679335b874d77773c33bb6f8af
panicbit <panicbit.dev@gmail.com>
2017-07-18 23:56:14 +0200

    Refactor to use message passing

--- src/main.rs
+++ src/main.rs
@@ -4,65 +4,60 @@
 extern crate rocket;
 extern crate ws;
 
-use std::sync::{Mutex, Arc};
+use std::sync::Mutex;
 use std::thread;
 use std::path::{PathBuf, Path};
 use rocket::*;
 use rocket::response::NamedFile;
+use ws::util::Token;
 
-use std::time::Instant;
-
+mod broker;
+use broker::{Broker,BrokerHandle};
 
 struct Handler {
-    clients: Arc<Mutex<Vec<ws::Sender>>>,
-    messages: Arc<Mutex<Vec<String>>>,
+    id: Token,
+    broker: BrokerHandle,
 }
 
 impl ws::Handler for Handler {
     fn on_message(&mut self, msg: ws::Message) -> ws::Result<()> {
-        self.messages.lock().unwrap().push(msg.to_string());
-        for client in &*self.clients.lock().unwrap() {
-            let _ = client.send(msg.clone());
-        }
+        self.broker.broadcast(self.id, msg.to_string());
         Ok(())
+    }
+
+    fn on_close(&mut self, _code: ws::CloseCode, _reason: &str) {
+        let _ = self.broker.disconnect(self.id);
     }
 }
 
 struct ChatServer {
-    clients: Arc<Mutex<Vec<ws::Sender>>>,
-    messages: Arc<Mutex<Vec<String>>>,
+    broker: Mutex<BrokerHandle>,
 }
 
 impl ChatServer {
     pub fn launch() -> Self {
-        let clients = Arc::new(Mutex::new(Vec::new()));
-        let messages = Arc::new(Mutex::new(Vec::new()));
+        let broker = Broker::spawn();
 
-        let clients1 = clients.clone();
-        let messages1 = messages.clone();
+        Self::spawn_websocket(broker.clone());
+
+        ChatServer {
+            broker: Mutex::new(broker),
+        }
+    }
+
+    fn spawn_websocket(broker: BrokerHandle) {
         thread::spawn(move || {
-            let clients = clients1;
-            let messages = messages1;
             ws::listen("localhost:8001", |sender| {
-                let _ = sender.send(format!("{:?} - History:", Instant::now()));
-                {
-                    let messages = messages.lock().unwrap().clone();
-                    for message in messages {
-                        let _ = sender.send(message);
-                    }
-                }
-                clients.lock().unwrap().push(sender);
+                let id = sender.token();
+
+                let _ = broker.connect(sender);
+
                 Handler {
-                    clients: clients.clone(),
-                    messages: messages.clone(),
+                    id: id,
+                    broker: broker.clone(),
                 }
             }).unwrap()
         });
-
-        ChatServer {
-            clients: clients,
-            messages: messages,
-        }
     }
 }
 
